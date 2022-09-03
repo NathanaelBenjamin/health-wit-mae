1cc7d6a172b819db73a27295b9a9030f1f9afbdc
Nathanael Benjamin <oluwakayodenathanael@gmail.com>
2022-09-03 16:59:01 +0100

    'Subscribe to newsletter' UI added

--- client/src/Pages/Home/JOIN-US/JoinUs.js
+++ client/src/Pages/Home/JOIN-US/JoinUs.js
@@ -4,7 +4,7 @@ import joinComponentData from './joinComponentData';
 
 const JoinUs = () => {
   return (
-    <div className='join-us grid grid-cols-1 md:grid-cols-2 gap-20 px-12 py-10 md:py-20 place-items-center'>
+    <div className='join-us grid grid-cols-1 md:grid-cols-2 gap-20 px-12 pt-10 pb-7 md:py-20 place-items-center'>
         { joinComponentData.map(item => {
           return <JoinComponents 
             heading={item.heading}
